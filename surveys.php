b5a763ffee5fe3138fd4309c04314621928b2a96
sarahlim26 <43418854+sarahlim26@users.noreply.github.com>
2020-04-26 18:36:09 -0400

    Update surveys.php

--- surveys.php
+++ surveys.php
@@ -1,83 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php include 'header';?>
+	<?php include 'header.php';?>
 <title>NJ forum Hub</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-<style>
-li {listt-style: none;}
-td {text-allign: center;}
 
-* {box-sizing: border-box;}
-
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.header {
-  overflow: hidden;
-  padding: 20px 10px;
-}
-
-header a {
-  float: left;
-  color: black;
-  text-align: center;
-  padding: 12px;
-  text-decoration: none;
-  font-size: 25px;
-  line-height: 25px;
-  border-radius: 4px;
-}
-
-.header a.logo {
-  font-size: 25px;
-  font-weight: bold;
-}
-
-.header a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.header a.active {
-  background-color: dodgerblue;
-  color: white;
-}
-
-.header-right {
-  float: right;
-}
-
-@media screen and (max-width: 500px) {
-  .header a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
- 
-  .header-right {
-    float: none;
-  }
-}
-
-</style>
 </head>
 
 <body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
 
-
-<div class="header">
-  <a href="#default" class="logo">NJ Forum Hub</a>
-  <div class="header-right">
-    <a class="active" href="showTables.php">Home</a>
-    <a href="createUser.php">New User</a>
-    <a href="surveys.php">Surveys</a>
-    <a href="forums.php">Forums</a>
-  </div>
-</div>
 
 <div class = "tabContainer">
 	<div class= "buttonContainer">
@@ -154,7 +86,7 @@ echo "</center>";
 
 ?>
 	<tr>
-	<td><?php echo $rows["Title"]; ?></td>
+  <td> <?php echo '<a href = "'.$rows["Title"].'">'.$rows["Title"].'</a><br>';?></td>
     <td><?php echo $rows["Length"]; ?></td>
     <td><?php echo $rows["Topic"]; ?></td>
     <td><?php echo $rows["Date_made"]; ?></td>
@@ -171,4 +103,3 @@ echo "</center>";
 
 </body>
 </html>
-
