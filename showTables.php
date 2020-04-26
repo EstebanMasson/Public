6caaa5353dad9358e0f3d9d09ad54fc2aa5ac0de
Tooba Bajwa <bajwat1@tcnj.edu>
2020-04-26 15:52:31 -0400

    Update showTables.php

--- showTables.php
+++ showTables.php
@@ -14,14 +14,22 @@ li {listt-style: none;}
 <h1 style="text-align:center" class="title">NJforum Website</h1>
 <div class = "tabContainer">
 	<div class= "buttonContainer">
+    <form action = "http://srhub.org">
+   		 <button type = "submit" >NJSR Hub Website</button>
+    </form>
+    <form action = "http://localhost/createUser.php">
+   		 <button type = "submit" >Register/Login</button>
+    </form>
 
-    	<form action = "http://localhost/createUser.php">
-    	<button type="submit">Go Back</button>   	 
+    	<form action = "http://localhost/surveys.php">
+    	<button type="submit">Surveys</button>   	 
 	</form>
+ 
 	</div>
 </div>
 
-<h1 style="text-align:center" class="title"> Forums </h1>
+<h1 style="text-align:center" class="title"> All Tables </h1>
+
 
 <?php
 $dbhost = "localhost";
@@ -33,13 +41,14 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 
 ?>
 
+
 <?php
 	$query = "SELECT * FROM FORUM_CATEGORIES";
 	$result = $conn->query($query);     	 
     
 ?>
 
-<table border = "1px" style= "width:600px; line-height:40px;">
+<table border = "1px" style= "width:1050px; line-height:40px;">
 
 <t>
 <th colspan = "8"> <h2>Forums </h2></th>
@@ -58,17 +67,22 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 <?php }
 ?>
 
+
+
+
+
 <?php
 	$query = "SELECT * FROM POSTS";
 	$result = $conn->query($query);     	 
     
 ?>
 
-<table border = "1px" style= "width:600px; line-height:30px;">
+<table border = "1px" style= "width:1050px; line-height:30px;">
 <t>
-<th colspan = "8"> <h2>Posts </h2></th>
+<th colspan = "15"> <h2>Posts</h2></th>
 
 </t>
+
 
 <tr>
 	<th> Author </th>
@@ -76,8 +90,7 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 	<th> Forum Title </th>
 	<th> Post </th>
     <th> Category </th>
-    <th> Day </th>
-    <th> Month </th>
+
     
 </tr>
 
@@ -91,15 +104,125 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
 	<td><?php echo $rows["fTitle"]; ?></td>
 	<td><?php echo $rows["Data"]; ?></td>
 	<td><?php echo $rows["Category"]; ?></td>
-	<td><?php echo $rows["pDay"]; ?></td>
-	<td><?php echo $rows["pMonth"]; ?></td>
+
 
 
 	</tr>
 <?php }
 ?>
 
+<?php
+	$query = "SELECT * FROM COMMENTS";
+	$result = $conn->query($query);     	 
+    
+?>
+
+<table border = "1px" style= "width:1050px; line-height:40px;">
+
+<t>
+<th colspan = "8"> <h2>Comments </h2></th>
+
+</t>
+
+<tr>
+	<th> Post Author </th>
+	<th> Post Time </th>
+	<th> Comment Author </th>
+	<th> Comment Time </th>
+    <th> Comment </th>
+    
+</tr>
+
+<?php
+	while($rows = $result -> fetch_assoc()){
+
+?>
+	<tr>
+	<td><?php echo $rows["pAuthor"]; ?></td>
+    <td><?php echo $rows["pTime"]; ?></td>
+    <td><?php echo $rows["cAuthor"]; ?></td>
+	 <td><?php echo $rows["cTime"]; ?></td>
+    <td><?php echo $rows["cData"]; ?></td>
+
+
+	</tr>
  
+<?php }
+?>
+
+
+<div style = "float: left;">
+<h2>Insert A Post</h2>
+<ul>
+<form name="insert" action="showTables.php" method="POST" autocomplete = "off" >
+
+
+<li>Your Name:</li><input type="text" name="postAuthor" />
+
+<li>Forum Title to Post in:</li><input type="text" name="fTitle" /> <br>
+<li>Post: </li><textarea rows="5" cols="50" name = 'Data'> Write your Post </textarea>
+<br>
+<input type="submit" value="submit"/>
+</form>
+</ul>
+</div>
+
+<div style = "float: left;">
+<h2>Insert A Comment</h2>
+<ul>
+<form name="insert" action="showTables.php" method="POST" autocomplete = "off" >
+<li>Post Author</li><input type = "text" name = "pAuthor"><br>
+<li>Your Name:</li><input type="text" name="cAuthor" />
+<br>
+<li>Comment: </li><textarea rows="5" cols="50" name = 'cData'> </textarea>
+<br>
+<input type="submit" value="submit"/>
+</form>
+</ul>
+</div>
+
+<?php
+if(isset($_POST['postAuthor']) && isset($_POST['fTitle']) && isset($_POST['Data']))
+{
+	$postAuthor = $_POST['postAuthor'];
+	$fTitle = $_POST['fTitle'];
+	$Data = $_POST['Data'];
+   
+
+	$sql = "INSERT INTO POSTS (postAuthor,fTitle,Data) VALUES ('$postAuthor', '$fTitle','$Data')";
+
+	if(mysqli_query($conn, $sql)){
+    	echo "Record added Succesfully";
+	} else {
+    	echo "ERROR: could not add record $sql" . mysqli_error($conn);
+	}
+
+	header("Location: showTables.php");
+	exit;
+}
+if(isset($_POST['cAuthor']) && isset($_POST['cData']))
+{
+	$pAuthor = $_POST['pAuthor'];
+	$cAuthor = $_POST['cAuthor'];
+	$cData = $_POST['cData'];
+
+ 
+	$sql = "INSERT INTO COMMENTS (pAuthor,cAuthor,cData) VALUES ('$pAuthor','$cAuthor', '$cData')";
+
+	if(mysqli_query($conn, $sql)){
+    	echo "Record added Succesfully";
+	} else {
+    	echo "ERROR: could not add record $sql" . mysqli_error($conn);
+	}
+
+	header("Location: showTables.php");
+	exit;
+}
+
+
+
+
+?>
 
 </table>
 
