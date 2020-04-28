2cf690a381e61606ec7ee2a3b42eb3e25032ab3a
sarahlim26 <43418854+sarahlim26@users.noreply.github.com>
2020-04-27 22:38:11 -0400

    Update headerForums.php

--- headerForums.php
+++ headerForums.php
@@ -1,59 +1,67 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-<title>NJ forum Hub</title>
+<title>NJforums</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+<body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
 <style>
 li {listt-style: none;}
-
+td {text-allign: center;}
+* {box-sizing: border-box;}
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.header {
+  overflow: hidden;
+  padding: 20px 10px;
+}
+header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 25px;
+  line-height: 25px;
+  border-radius: 4px;
+}
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+.header-right {
+  float: right;
+}
+@media screen and (max-width: 500px) {
+  .header a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+ 
+  .header-right {
+    float: none;
+  }
+}
 </style>
 </head>
 
-<body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
-<h1 style="text-align:center" class="title">NJforum Website</h1>
-
-
-
-<?php
-require ('headerForums.php');
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "NJforum";
-
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
-
-?>
-
-<?php
-	$query = "SELECT * FROM FORUM_CATEGORIES";
-	$result = $conn->query($query);     	 
-    
-?>
-
-<table border = "1px" style= "width:600px; line-height:40px;">
-
-<t>
-<th colspan = "8"> <h2>Forums </h2></th>
-
-</t>
-
-
-<?php
-	while($rows = $result -> fetch_assoc()){
-
-?>
-	<tr>
-	<td class = "mdc-data-table_cell"><?php echo '<a target = "_blank" href = '.$rows["Title"].'">'.$rows["Title"].'</a> <br>';?></td>
-	</tr>
- 
-<?php }
-?>
-
-
-</table>
-
-
-</body>
-</html>
+<div class="header">
+  <a href="#default" class="logo">NJ Forum Hub</a>
+  <div class="header-right">
+    <a class="active" href="index.php">Home</a>
+    <a href="register.php">Register/Sign In</a>
+    <a href="forums.php">Forums</a>
+    <a href="posts.php">Posts</a>
+  </div>
+</div>
