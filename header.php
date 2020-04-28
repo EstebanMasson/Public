0512a45a2adcdbd52c0043fa30cad39364035a67
sarahlim26 <43418854+sarahlim26@users.noreply.github.com>
2020-04-27 22:11:57 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,54 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
+<title>NJforums</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+<body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
 <style>
 li {listt-style: none;}
 td {text-allign: center;}
-
 * {box-sizing: border-box;}
-
 body {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
-
 .header {
   overflow: hidden;
   padding: 20px 10px;
 }
-
 header a {
   float: left;
   color: black;
   text-align: center;
   padding: 12px;
   text-decoration: none;
-  font-size: 50px;
+  font-size: 25px;
   line-height: 25px;
   border-radius: 4px;
 }
-
 .header a.logo {
   font-size: 25px;
   font-weight: bold;
 }
-
 .header a:hover {
   background-color: #ddd;
   color: black;
 }
-
 .header a.active {
   background-color: dodgerblue;
   color: white;
 }
-
 .header-right {
   float: right;
 }
-
 @media screen and (max-width: 500px) {
   .header a {
     float: none;
@@ -60,16 +52,16 @@ header a {
     float: none;
   }
 }
-
 </style>
 </head>
 
 <div class="header">
-  <a href="index.php" class="logo">NJ Forum Hub</a>
+  <a href="#default" class="logo">NJ Forum Hub</a>
   <div class="header-right">
     <a class="active" href="index.php">Home</a>
-    <a href="createUser.php">New User</a>
+    <a href="register.php">Register/Sign In</a>
     <a href="surveys.php">Surveys</a>
     <a href="forums.php">Forums</a>
+    <a href="posts.php">Posts</a>
   </div>
 </div>
