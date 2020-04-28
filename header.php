e803d0da35da2c47d9e8a87e954acd3bd27d040c
Svanik Shirodkar <svanikss@gmail.com>
2020-04-27 22:18:03 -0400

    Update header.php

--- header.php
+++ header.php
@@ -2,57 +2,13 @@
 <html>
 <head>
 <title>NJforums</title>
+  
+// CSS Link
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
-<style>
-li {listt-style: none;}
-td {text-allign: center;}
-* {box-sizing: border-box;}
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
-.header {
-  overflow: hidden;
-  padding: 20px 10px;
-}
-header a {
-  float: left;
-  color: black;
-  text-align: center;
-  padding: 12px;
-  text-decoration: none;
-  font-size: 25px;
-  line-height: 25px;
-  border-radius: 4px;
-}
-.header a.logo {
-  font-size: 25px;
-  font-weight: bold;
-}
-.header a:hover {
-  background-color: #ddd;
-  color: black;
-}
-.header a.active {
-  background-color: dodgerblue;
-  color: white;
-}
-.header-right {
-  float: right;
-}
-@media screen and (max-width: 500px) {
-  .header a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
- 
-  .header-right {
-    float: none;
-  }
-}
-</style>
+
 </head>
 
 <div class="header">
