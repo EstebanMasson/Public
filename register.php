65c874b49c2a2a4982fdc8e8eaeecf0ef0e9797a
EstebanMasson <44709844+EstebanMasson@users.noreply.github.com>
2020-04-29 18:51:08 -0400

    Add files via upload

--- register.php
+++ register.php
@@ -1,103 +1,120 @@
 <!DOCTYPE html>
-<html>
 <head>
-<?php include 'header';?> // get the header file to be displayed on the website 
+
+<!--Background and header tab navigation -->
+<?php include 'header.php';?>
+	
+<title>NJ forum Hub</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
 <style>
+
 li {listt-style: none;}
 
 </style>
+
 </head>
 
-<!--background picture -->
-<body style = "background: url(https://www.siarza.com/wp-content/uploads/2017/12/webplunder-background-image-technology-online-website-solutions.jpg);">
+<h1 style="text-align:center" class="title">NJforum Website</h1>
 
-<h1 style="text-align:center" class="title">NJforum Website</h1> <!--Title of our website-->
-<html>
-   
-   <head>
-      <title>Login Page</title>
-      
-   </head>
-   
-   <body bgcolor = "#FFFFFF">
-	<div class = "tabContainer">
-    <div class= "buttonContainer">
-    <form action = "http://srhub.org">
-   		 <button type = "submit" >NJSR Hub Website</button> <!--NJSR Hub button links to the NJSRHub website-->
-    </form>
-    <form action = "http://localhost/forums.php">
-   		 <button type = "submit" >Forums</button> <!--Forum button links to the forums.php file-->
-    </form>
+<!--Buttons to access original website and go back to previous page-->
+<div class = "tabContainer">
+
+	<div class= "buttonContainer">
+
+   <form action = "http://srhub.org">
+
+       	<button type = "submit" >NJSR Hub Website</button>
+
+   </form>
+
+   	<form action = "http://localhost/forums.php">
+
+       	<button type = "submit" >Forums</button>
+
+   </form>
+
 	</div>
-</div>
 
-      <div>
-         <div style = "width:300px; " align = "left">
-            <h1>Sign In</h1>
-				
-            <div style = "margin:30px">
-               
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br /> <!--user insertion-->
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br /> <!--password insertion-->
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-               
-              
-					
-            </div>
-				
-         </div>
-			
-      </div>
+</div style = "float: left;">
 
+<h2>Enter New User Data</h2>
 
+<ul>
 
-   </body>
-</html>
+<!--Form for user to create new account-->
+<form name="insert" action="createUser.php" method="POST" autocomplete = "off">
+
+<li>First Name:</li><input type="text" name="fName" />
+
+<li>Last Name:</li><input type="text" name="lName" />
+
+<li>Username:</li><input type="text" name="Username" />
+
+<li>Password:</li><input type="password" name="Password" />
+
+<br>
+
+<!--Submit user input to database-->
+<input type="submit" value="submit"/>
+
+<!-- Login page link -->
+<p> Already have an account? <a href="login.php"> Login here </a>.</p>
+
+</form>
+
+</ul>
+
 
 <?php
-require_once ('users.php'); // registering a user into the database
+//connect to databse
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$db = "NJforum";
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+
+//check if user input is valid
+if(!(empty($_POST['Username']) || empty($_POST['Password']) || empty($_POST['fName']) || empty($_POST['fName'])))
+{
+	$Username = $_POST['Username'];
+	$Password = $_POST['Password'];
+	$fName = $_POST['fName'];
+	$lName = $_POST['lName'];
+	
+	//check if username being made is not already made
+	$query = "SELECT * FROM USERS WHERE Username = '$Username'";
+	$result = mysqli_query($conn, $query);
+	if(mysqli_num_rows($result)>0)
+		echo "ERROR: Username Already Exist";
+
+	else
+	{
+		//username is valid create new user to database
+		$sql = "INSERT INTO USERS (Username, Password, fName, lName) VALUES
+			('$Username','$Password','$fName','$lName')";
+
+	if(mysqli_query($conn, $sql))
+		echo "Record added Succesfully";
+	else
+		echo "ERROR: could not add record $sql" . mysqli_error($conn);
+
+	//if every query ran correctly proceed to showTables page
+	header("Location: showTables.php");
+	exit();
+	}
+}
+
+//as long as the fields are not fully filled out then keepp tellin user to fill it out
+if(empty($Username) || empty($Password) || empty($fName) || empty($lName)){
+
+	echo "All fields are Mandatory\r\n";
+	exit;
+}
 ?>
-
-<!--creating a new user-->
-<?php
-
-   session_start();
-// connecting to the database
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpass = "";
-  $db = "NJforum";
-
-  $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
-   if($_SERVER["REQUEST_METHOD"] == "USERS") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$USERS['Username']);
-      $mypassword = mysqli_real_escape_string($db,$USERS['Password']); 
-      
-      $sql = "SELECT Username FROM USERS WHERE Username = '$myusername' and Password = '$mypassword'"; // find user in the database 
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1 && mysqli_query($conn, $sql)) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("Location: http://localhost/showTables.php"); // redirects to the showTables.php if user is found 
-      }else {
-         $error = "Your Login Name or Password is invalid"; // notify user of invalid login information
-      }
-   }
-?>
-
-
-
+<br />
+<br />
+</table>
 </body>
 </html>
